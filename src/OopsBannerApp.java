d14e64cdbc7ddba08d936ed52385672c13d61450
unknown <d46518319@gmail.com>
2026-03-06 11:07:31 +0530

    Coded UC1 to Print OOPS to Console

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,6 +1,6 @@
 class OopsBannerApp {
     // print oops banner
     public static void main(String[] args) {
-        System.out.println("Welcome to OOPS Banner App!");
+        System.out.println("OOPS");
     }
 }
