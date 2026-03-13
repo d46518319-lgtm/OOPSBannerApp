0f70baa173b3d61f0a4583bb9b9514341584f612
unknown <d46518319@gmail.com>
2026-03-13 10:42:17 +0530

    feature/uc2

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -2,13 +2,12 @@ class OopsBannerApp {
     // print oops banner
     public static void main(String[] args) {
         
-        System.out.println("***    ***    *****    *****");
-        System.out.println("*  *  *  *    *   *    *");
-        System.out.println("*  *  *  *    *   *    *");
-        System.out.println("*  *  *  *    *****    *****");
-        System.out.println("*  *  *  *    *            *");
-        System.out.println("*  *  *  *    *            *");
-        System.out.println("***    ***    *        *****");
-
+          System.out.println(String.join(" ", "   ***   ", "   ***   ", "******  ", "   ***** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    **", " **      "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**   ** ", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**    **", "  *****  "));
+        System.out.println(String.join(" ", "**     **", "**     **", "******  ", "       **"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**      ", "**     **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", "**      ", "  *****  "));
     }
 }
